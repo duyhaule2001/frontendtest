fe979c2eba6924166c081ba2ca36099f8dd9c47e
duyhaule2001 <duyhaule2001@gmail.com>
2024-11-20 13:32:44 +0900

    up

--- vite.config.js
+++ vite.config.js
@@ -7,25 +7,5 @@ export default defineConfig({
     build: {
         outDir: 'dist',
         emptyOutDir: true,
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        if (id.includes('react')) {
-                            return 'vendor-react';
-                        }
-                        if (id.includes('antd')) {
-                            return 'vendor-antd';
-                        }
-                        if (id.includes('lodash')) {
-                            return 'vendor-lodash';
-                        }
-                        return 'vendor';
-                    }
-                },
-            },
-        },
-        assetsInclude: ['**/*.xlsx'],
-        chunkSizeWarningLimit: 2000,
     },
 });
