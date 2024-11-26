87ddd350aa38d92aedd96310589433606e8844bc
duyhaule2001 <duyhaule2001@gmail.com>
2024-11-26 16:16:23 +0900

    up

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     build: {
-        outDir: 'dist',
+        outDir: '../skytech_backend/static/',
         emptyOutDir: true,
-    },
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://sky-tech.cloud', // Địa chỉ HTTP backend
-                changeOrigin: true, // Thay đổi origin để phù hợp với backend
-                secure: false, // Bỏ qua lỗi SSL nếu có
-                rewrite: (path) => path.replace(/^\/api/, ''), // Xóa prefix '/api' nếu cần
-            },
-        },
     },
 });
